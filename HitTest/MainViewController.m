43357b1abc23b014b20d29c8cbb0776109f4b87f
陈宇峰 <yufengcode@gmail.com>
2016-07-04 09:42:40 +0800

    init

---
+++ HitTest/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HitTest
+//
+//  Created by chenyufeng on 7/4/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
