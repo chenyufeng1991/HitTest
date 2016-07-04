9281f45f8f22edecbf2d5d9fc0d03712b6f99877
陈宇峰 <yufengcode@gmail.com>
2016-07-04 09:37:00 +0800

    Initial Commit

---
+++ HitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HitTest
+//
+//  Created by chenyufeng on 7/4/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HitTest
+//
+//  Created by chenyufeng on 7/4/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
